e2b4c6c9efce501f91b6ede7cf431c11384b66b0
azeemsm-umdctf <azeemsm.umdctf@gmail.com>
2021-04-07 02:31:42 -0400

    different hook to only send me email

--- program.py
+++ program.py
@@ -4,7 +4,6 @@ import requests
 
 f=open("maker.key","r")
 secret_key=f.read().strip()
-my_data = { "value1" : "azeemsm.umdctf@gmail.com", "value2" : "Azeem" }
 
 arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 print("running...")
@@ -16,7 +15,7 @@ while True:
             message = data.decode('UTF-8').rstrip('\n')
             if "ring!" in message:
                 #send request to webhooks so ifttt can do its thing!
-                r=requests.post("https://maker.ifttt.com/trigger/ring/with/key/"+secret_key, data=my_data)
+                r=requests.post("https://maker.ifttt.com/trigger/ring2/with/key/"+secret_key)
                 print(r.text)
                 
     except:
