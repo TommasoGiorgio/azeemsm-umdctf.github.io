9f3edf66e1f0965eca09a94bccfe454951330189
azeemsm-umdctf <82078219+azeemsm-umdctf@users.noreply.github.com>
2021-04-13 00:40:05 -0400

    Update doorbell_arduino.ino
    updated pin number and removed cringe default comments

--- doorbell_arduino/doorbell_arduino.ino
+++ doorbell_arduino/doorbell_arduino.ino
@@ -1,13 +1,11 @@
-const int bellPin = 5;
+const int bellPin = 6;
 void setup() {
-  // put your setup code here, to run once:
   pinMode(bellPin, INPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int bellState = digitalRead(bellPin);
   if(bellPin){
     Serial.println("ring!");
